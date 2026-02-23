3373206190a5ee524abbd840fbbf3d4014a9ef37
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-02-22 21:30:34 -0600

    print err

--- bar.zig
+++ bar.zig
@@ -40,10 +40,35 @@ pub fn main() !void {
                 );
             }
         },
-        .err => @panic("TODO: err"),
+        .err => |err| {
+            var tokenizer = err.data.tokenizer;
+            const line_no, const column = .{ tokenizer.line-1, tokenizer.column-1 };
+            const line = og_lines.items[line_no];
+            const tok = tokenizer.token_buffer.items[0..];
+            std.debug.print(
+                "\x1b[1;31mERR:\x1b[22m {t} "
+                    ++ "\x1b[3;35m(line \x1b[4;36m{d}\x1b[24;35m, "
+                    ++ "column \x1b[4;36m{d}\x1b[24;35m)\x1b[0m\n",
+                .{err.value.?, line_no, column }
+            );
+            const pre = line[0..line.len-tok.len];
+            std.debug.print("\t{s}\x1b[5;1;33m{s}\n", .{ pre, line[pre.len..pre.len+tok.len] });
+            std.debug.print("\t\x1b[34m{s}\x1b[0m\n", .{ b: {
+                var res = try std.ArrayList(u8).initCapacity(alloc, 0);
+                defer res.deinit(alloc);
+                var do_underline = false;
+                for (0..column-tok.len) |i| {
+                    do_underline = if (!do_underline and line[i] == ' ') false else true;
+                    try res.append(alloc, if (do_underline) '^' else ' ');
+                }
+                try res.appendSlice(alloc, "\x1b[1;36m");
+                for (0..tok.len) |_| try res.append(alloc, '^');
+                break :b try res.toOwnedSlice(alloc);
+            }});
+        },
     }
 
-    std.debug.print("main memory leakage: {d}\n", .{arena.state.end_index});
+    std.debug.print("nodes in main arena: {d}\n", .{arena.state.end_index});
     if (!arena.reset(.free_all)) @panic("failed to reset arena");
 
     arena.deinit();
@@ -83,7 +108,10 @@ fn initial_validation(
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const alloc = arena.allocator();
     //leak a tonne of memory then reset arena when fn returns 
-    defer { _ = arena.reset(.free_all); arena.deinit(); }
+    defer {
+        std.debug.print("nodes in initial validation arena: {d}\n", .{arena.state.end_index});
+        _ = arena.reset(.free_all); arena.deinit();
+    }
 
     //initialize a stream iterator for the KDL source 
     var reader = std.Io.Reader.fixed(source);
